6f60cf8c67eec32844cfd415bcb89ba1b70d422b
Cole <coledavenport2010@outlook.com>
2026-08-28 09:53:58 -0500

    website

---
+++ sidepanel.js
@@ -0,0 +1,12 @@
+function togglePanel(){
+  var panel= document.getElementById('side-panel')
+  if (!panel.___closed){
+    emile(panel, 'widith:0px')
+    panel.___closed = true;
+    
+  }
+  }else{
+  emile(panel, 200px);
+  panel._____closed = false 
+    }
+}
